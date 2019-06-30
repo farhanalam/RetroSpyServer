b2313fad41b8712cc52096fe6300a39db9f55777
XIAOJIUWO <koujiangheng@live.cn>
2019-07-01 02:46:20 +0800

    create projects for separatng the retrospy servers to each single project

---
+++ Servers/GameSpyCDKeyServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameSpyCDKeyServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
